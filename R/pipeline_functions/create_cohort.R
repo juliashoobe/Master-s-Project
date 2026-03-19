a81ce6f1d6142d3fa95d7c52a9b7a1a93ef21c92
juliashoobe <70176735+juliashoobe@users.noreply.github.com>
2026-03-19 11:55:30 -0400

    Update create_cohort.R

--- R/pipeline_functions/create_cohort.R
+++ R/pipeline_functions/create_cohort.R
@@ -1,12 +1,12 @@
-create_cohort <- function(encounters_df, labs_df, notes_df) {
+create_cohort <- function(ids, encounters_df, labs_df, notes_df) {
   
   # Step 1: Keep only high-sensitivity troponin labs relevant to protocol
   # (baseline, 1-hour, 3-hour, and retest)
   lab_values <- labs_df |> 
     filter(
+      ENCOUNTERID %in% ids &
       RAW_LAB_NAME %in% c("hsTnI Baseline", "hsTnI 1 hr", "hsTnI 3 hr", 
-                          "hsTnI Retest"
-    )
+                          "hsTnI Retest")
     )
   
   # Step 2: Create an encounter-level troponin abnormality flag
@@ -22,12 +22,10 @@ create_cohort <- function(encounters_df, labs_df, notes_df) {
   # Step 3: Build cohort
   cohort <- encounters_df |> 
     select(-PATID) |> 
+    filter(ENCOUNTERID %in% ids) |> 
     
     # Restrict encounters that have qualifying troponin labs
     inner_join(lab_values, by = "ENCOUNTERID") |> 
-    
-    # Restrict to encounters that have clinical notes available
-    filter(ENCOUNTERID %in% notes_df$ENCOUNTERID) |> 
     
     # Within each encounter, select the single highest troponin value
     group_by(ENCOUNTERID) |> 
