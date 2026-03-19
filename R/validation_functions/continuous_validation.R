17c85a989937a4c736dfc719c3a7bfaff1ffa297
juliashoobe <70176735+juliashoobe@users.noreply.github.com>
2026-03-19 12:16:15 -0400

    Update continuous_validation.R

--- R/validation_functions/continuous_validation.R
+++ R/validation_functions/continuous_validation.R
@@ -1,14 +1,15 @@
 continuous_validation <- function(chart_df, pipeline_df, variable) {
   
-  # mean squared error
-  mse <- mse(chart_df[[variable]], pipeline_df[[variable]])
+  vars <- prepare_validation_data(chart_df, pipeline_df, variable)
   
-  # mean absolute error
-  mae <- mae(chart_df[[variable]], pipeline_df[[variable]])
+  valid <- complete.cases(vars$chart, vars$pipe)
   
   result <- list(
-    MSE = mse,
-    MAE = mae
+    # mean square error
+    MSE = mse(vars$chart[valid], vars$pipe[valid]),
+    
+    # mean absolute error
+    MAE = mae(vars$chart[valid], vars$pipe[valid])
   )
   
   return(result)
